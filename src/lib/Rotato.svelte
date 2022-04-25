52ddc604c6df3c247b5da8b37532d925b8c91121
Sarah-W <sarah.wheaton@gmail.com>
2022-04-25 13:36:36 +1200

    moved skirttypes generator to its own file

---
+++ src/lib/Rotato.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let height =10
+  export let width = 10
+  export let x = 10
+  export let y = 0
+  export let rot = 0
+
+  const clickhandler = ()=>{
+    rot=rot+10
+  }
+</script>
+
+<rect 
+  on:click={clickhandler} 
+  height={height} 
+  width={width} 
+  transform={`translate(${x},${y}) rotate(${rot})`}>
+</rect>
+
+<style lang="scss">
+  rect{
+    fill:bisque;
+    stroke:brown;
+  }
+</style>
